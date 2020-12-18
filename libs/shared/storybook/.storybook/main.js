b2b1c2525a516c627ba9094de1bdb5d95b80b773
Nathan Samsoedien <samsoedien@Nathans-MacBook-Pro.local>
2020-12-18 23:10:29 +0100

    chore: add storybook addon-a11y

--- libs/shared/storybook/.storybook/main.js
+++ libs/shared/storybook/.storybook/main.js
@@ -1,7 +1,7 @@
 const rootMain = require('../../../../.storybook/main');
 
 // Use the following syntax to add addons!
-rootMain.addons.push('@storybook/addon-essentials');
+rootMain.addons.push(...['@storybook/addon-essentials', '@storybook/addon-a11y']);
 rootMain.stories.push(
   ...['../../**/*.stories.mdx', '../../**/*.stories.@(js|jsx|ts|tsx)']
 );
